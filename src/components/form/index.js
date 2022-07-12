0ee0713025bc7b52968e418e9cf019c73c5f426a
Filipe Alves Braga Rosa <54287231+FilipeAlvesPRG@users.noreply.github.com>
2022-07-12 19:22:16 -0300

    Update app style

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
-import { View, Text, TextInput, Button } from "react-native"
+import { View, Text, TextInput, TouchableOpacity } from "react-native"
 import ResultImc from "./resultImc";
+import styles from "./style";
 
 export default function Form() {
 
@@ -30,28 +31,32 @@ export default function Form() {
 
 
     return (
-        <View>
-            <View>
-                <Text>Altura</Text>
+        <View style={styles.formContext}>
+            <View style={styles.form}>
+                <Text style={styles.formLabel}>Altura</Text>
                 <TextInput
+                    style={styles.formInput}
                     onChangeText={setHeight}
                     value={height}
                     placeholder="Ex. 1.75"
                     keyboardType="numbers-and-punctuation"
                 />
-
-                <Text>Peso</Text>
+                <Text style={styles.formLabel}>Peso</Text>
                 <TextInput
+                    style={styles.formInput}
                     onChangeText={setWeight}
                     value={weight}
                     placeholder="Ex. 75.10"
-                    keyboardType="numeric"
+                    keyboardType="numbers-and-punctuation"
                 />
-                <Button
-                    onPress={() => validationImc()
-                    }
-                    title={textButton}
-                />
+
+                <TouchableOpacity
+                    style={styles.formButtonCalculator}
+                    onPress={() => validationImc()}
+                >
+                    <Text style={styles.textButtonCalculator}>{textButton}</Text>
+                </TouchableOpacity>
+
             </View>
             <ResultImc
                 messageResultImc={messageImc}
--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { View, Text } from "react-native"
+import styles from "./style";
 
-export default function Title () {
-    return(
-        <View>
-            <Text> HealthApp </Text>
+export default function Title() {
+    return (
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}> IMC Calculator </Text>
         </View>
     );
 }
